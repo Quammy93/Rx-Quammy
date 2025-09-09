ec351a78e0d089f264d0261439ed09ae5d6ba390
Maxwell Ahorlu <quammy93@gmail.com>
2025-09-09 15:38:12 +0000

    Fix cleanup script to resolve EIO error

--- cleanup.sh
+++ cleanup.sh
@@ -4,6 +4,13 @@ rm -f styles.css
 rm -f script.js
 rm -f index.html
 
+# Remove node_modules and package-lock.json to ensure clean state
+rm -rf node_modules
+rm -f package-lock.json
+
+# Clear npm cache to remove any corrupted cached files
+npm cache clean --force
+
 # Install dependencies
 npm install
 
